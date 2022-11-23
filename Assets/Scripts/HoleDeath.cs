8b6499b601f460523340bc2f25049093397e7d3b
Daniel-Cairney <114543733+Daniel-Cairney@users.noreply.github.com>
2022-11-23 12:25:29 +1100

    collision

--- Assets/Scripts/HoleDeath.cs
+++ Assets/Scripts/HoleDeath.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class HoleDeath : MonoBehaviour
+namespace CairneyDaniel
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public class HoleDeath : MonoBehaviour
+    {   
+        public GameObject Hole;
+        private void OnCollisionEnter(Collision collision)
+        {
+
+            Debug.Log("Collision");
+            SceneManager.LoadScene("Death Screen");
+        }
+
     }
 }
